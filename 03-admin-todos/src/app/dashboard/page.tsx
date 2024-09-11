6d76652342f4fe007aace0b2a44561486cc89135
Strocs <strocsdev@gmail.com>
2024-09-11 00:51:21 -0300

    add: dashboard private route

--- 03-admin-todos/src/app/dashboard/page.tsx
+++ 03-admin-todos/src/app/dashboard/page.tsx
@@ -1,11 +1,21 @@
-import { WidgetItem } from '@/components'
+import { WidgetItem } from "@/components";
+import { auth } from "@/lib/auth";
+import { redirect } from "next/navigation";
 
-export default function DashboardPage () {
+export default async function DashboardPage() {
+  const session = await auth();
+
+  if (!session) {
+    redirect("/api/auth/signin");
+  }
+
   return (
-    <div className='grid gap-6 md:grid-cols-2 lg:grid-cols-3'>
-      <WidgetItem title='Dashboard'>
-        <div></div>
+    <div className="grid gap-6 md:grid-cols-2 lg:grid-cols-3">
+      <WidgetItem title="Usuario conectado Server Side">
+        <div>
+          <p>{session.user?.name}</p>
+        </div>
       </WidgetItem>
     </div>
-  )
+  );
 }
